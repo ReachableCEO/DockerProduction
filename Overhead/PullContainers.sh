1ed8838270297206c7ac81f07424aec8bd8db80a
Charles N Wyble <charles@turnsys.com>
2022-03-14 13:33:59 -0500

    automation here we come...

---
+++ Overhead/PullContainers.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export container_list="
+        jenkins/jenkins \
+        elabftw/elabimg \
+        huginn/huginn \
+        phpipam/phpipam-www \
+        photoprism/photoprism \
+        deamos/openstreamingplatform \
+        securecompliance/gvm:debian-master-data-full \
+        securecompliance/gvm:debian-master-data \
+        securecompliance/gvm:debian-master-full \
+        securecompliance/gvm:debian-master \
+        killbill/killbill \
+        killbill/kaui \
+        drone/drone \
+        archivebox/archivebox \
+        apache/tika \
+        thecodingmachine/gotenberg \
+        inventree/inventree \
+        jonaswinkler/paperless-ng \
+        seknox/guacd \
+        seknox/trasa \
+        bunkerity/bunkerized-nginx \
+        linuxserver/swag \
+        authelia/authelia \
+        beanbag/reviewboard:latest \
+        containrrr/watchtower:latest \
+        xetusoss/archiva
+        "
+
+for container in $container_list;
+do
+        docker pull $container
+done
