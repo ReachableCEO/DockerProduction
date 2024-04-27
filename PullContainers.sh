2a3761a559e574158c357bcd627b9f2ff1931a23
Charles Wyble <charles@turnsys.com>
2024-04-27 14:08:02 -0500

    refactored to use external files and per host

--- PullContainers.sh
+++ PullContainers.sh
@@ -1,43 +1,6 @@
 #!/bin/bash
-export container_list="
-        elabftw/elabimg \
-        huginn/huginn \
-        phpipam/phpipam-www \
-        killbill/killbill \
-        killbill/kaui \
-        inventree/inventree \
-        seknox/trasa \
-        beanbag/reviewboard:latest \
-        pihole/pihole \
-        stedolan/jq \
-        r7wx/easy-gate \
-        rundeck/rundeck \
-        librenms/librenms \
-        graylog/graylog:5.1 \
-        r7wx/easy-gate \
-        xetusoss/archiva"
 
-for container in $container_list;
+for container in $(cat ./($hostname -s)/$(hostname -s)-containers.txt;
 do
-        docker pull $container &
+        docker pull $container 
 done
-
-#Mailpile
-#easyforms (not a docker container)
-
-#TSYS3 (usb passthrough)
-#git subtree add --prefix upstream/cloudflare-cfssl  https://github.com/rjrivero/docker-cfssl.git master --squash
-#git subtree add --prefix upstream/cloudflare-certmgr https://github.com/cloudflare/certmgr.git master --squash
-
-#Need to deep dive...
-#git subtree add --prefix upstream/wazuh https://github.com/wazuh/wazuh-docker.git master --squash
-#git subtree add --prefix upstream/openvas https://github.com/mikesplain/openvas-docker.git  master --squash
-#        greenbone/vulnerability-tests \
-#        greenbone/notus-data \
-#        greenbone/scap-data \
-#        securecompliance/gvm:debian-master-data-full \
-#        securecompliance/gvm:debian-master-data \
-#        securecompliance/gvm:debian-master-full \
-#        securecompliance/gvm:debian-master \
-#mailman
-
